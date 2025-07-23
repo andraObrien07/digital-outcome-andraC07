c20dc2aa7d6834b4c6a22ce0edf41646b4c6c17d
andraObrien07 <comanan09@craighead.school.nz>
2025-07-24 09:28:20 +1200

    reloading issue
    when I reload my page it no longer rememebrs me. If i simply logout then back in it does remember. I have no idea why. I think something is not correct with my on-mount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,23 @@
     import Header from "$lib/Header.svelte";
     import { login } from "$lib/db.js";
     import { goto } from "$app/navigation";
-    import { onMount } from "svelte";
-    import { user } from "$lib/state.svelte.js";
+    // import { onMount } from "svelte";
+    // import { user } from "$lib/state.svelte.js";
+    // import { getfarms } from "$lib/db.js";
 
-    onMount(() => {
-        if (localStorage.getItem("user") && !user.uid) {
-            let data = JSON.parse(localStorage.getItem("user"));
-            console.log("Loading user from localStorage:", data);
-            user.uid = data.uid;
-            user.email = data.email;
-            user.displayName = data.displayName;
-            user.photoURL = data.photoURL;
-            console.log("User loaded from localStorage:", user);
-            // getCharacter(data.uid);
-        }
-    });
+    // onMount(() => {
+    //     if (localStorage.getItem("user") && !user.uid) {
+    //         let data = JSON.parse(localStorage.getItem("user"));
+    //         console.log("Loading user from localStorage:", data);
+    //         user.uid = data.uid;
+    //         user.email = data.email;
+    //         user.displayName = data.displayName;
+    //         user.photoURL = data.photoURL;
+    //         console.log("User loaded from localStorage:", user);
+    //         // getCharacter(data.uid);
+    //         getfarms();
+    //     }
+    // });
 </script>
 
 <Header />
--- src/routes/yourFarm/+page.svelte
+++ src/routes/yourFarm/+page.svelte
@@ -4,6 +4,22 @@
     import { logout } from "$lib/db.js";
     import { goto } from "$app/navigation";
     import { landScape } from "$lib/state.svelte.js";
+    import { onMount } from "svelte";
+
+    import { getfarms } from "$lib/db.js";
+    onMount(() => {
+        if (localStorage.getItem("user") && !user.uid) {
+            let data = JSON.parse(localStorage.getItem("user"));
+            console.log("Loading user from localStorage:", data);
+            user.uid = data.uid;
+            user.email = data.email;
+            user.displayName = data.displayName;
+            user.photoURL = data.photoURL;
+            console.log("User loaded from localStorage:", user);
+            // getCharacter(data.uid);
+            getfarms();
+        }
+    });
 </script>
 
 <Header />
@@ -19,15 +35,17 @@
         logout
     </button>
 
-    <button
-        class="button"
-        on:click={() => {
-            goto("/choicefarm");
-        }}
-    >
-        <p>{landScape.nameFarm}</p>
-        <!-- <p>{user.uid}</p> -->
-    </button>
+    {#if user.uid}
+        <button
+            class="button"
+            on:click={() => {
+                goto("/choicefarm");
+            }}
+        >
+            <p>{landScape.nameFarm}</p>
+            <!-- <p>{user.uid}</p> -->
+        </button>
+    {/if}
     <button
         class="button"
         on:click={() => {
